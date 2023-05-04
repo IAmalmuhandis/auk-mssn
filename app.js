f801e2bd75b4aff4146e2ccff2bbc0c6d1e5fa45
Abubakar Almuhandis <89389485+IAmalmuhandis@users.noreply.github.com>
2023-05-04 14:03:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+const _ = require('lodash');
+const path = require('path');
+
+const {Donor} = require('./models/donor')
+const {initializePayment, verifyPayment} = require('./config/paystack')(request);
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public/')));
+app.set('view engine', pug);
+
+app.get('/',(req, res) => {
+    res.render('index.pug');
+});
+
+app.post('/paystack/pay', (req, res) => {
+    const form = _.pick(req.body,[
+        'amount',
+        'email',
+        'full_name',
+        'regno',
+        'phone',
+        'course',
+        'level'
+    ]);
+    form.metadata = {
+        full_name : form.full_name,
+        regno: form.regno,
+        phone: form.phone,
+        course: form.course,
+        level: form.level
+    }
+    form.amount *= 100;
+    form.subaccount = 'ACCT_o92krz6yfv87xjf';
+    
+    initializePayment(form, (error, body)=>{
+        if(error){
+            //handle errors
+            console.log(error);
+            return res.redirect('/error')
+            return;
+        }
+        response = JSON.parse(body);
+        res.redirect(response.data.authorization_url)
+    });
+});
+
+app.get('/paystack/callback', (req,res) => {
+    const ref = req.query.reference;
+    verifyPayment(ref, (error,body)=>{
+        if(error){
+            //handle errors appropriately
+            console.log(error)
+            return res.redirect('/error');
+        }
+        response = JSON.parse(body);        
+
+        const data = _.at(response.data, [
+            'reference',
+            'amount',
+            'customer.email',
+            'metadata.full_name',
+            'metadata.regno',
+            'metadata.phone',
+            'metadata.course',
+            'metadata.level'
+        ]);
+
+        [
+            reference,
+            amount,
+            email,
+            full_name,
+            regno,
+            phone,
+            course,
+            level
+        ] =  data;
+        
+        newDonor = {
+            reference,
+            amount,
+            email,
+            full_name,
+            regno,
+            phone,
+            course,
+            level
+        }
+
+        const donor = new Donor(newDonor)
+
+        donor.save().then((donor)=>{
+            if(!donor){
+                return res.redirect('/error');
+            }
+            res.redirect('/receipt/'+donor._id);
+        }).catch((e)=>{
+            res.redirect('/error');
+        })
+    })
+});
+
+app.get('/receipt/:id', (req, res)=>{
+    const id = req.params.id;
+    Donor.findById(id).then((donor)=>{
+        if(!donor){
+            res.redirect('/error')
+        }
+        res.render('success.pug',{donor});
+    }).catch((e)=>{
+        res.redirect('/error')
+    })
+})
+
+app.get('/error', (req, res)=>{
+    res.render('error.pug');
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+});
